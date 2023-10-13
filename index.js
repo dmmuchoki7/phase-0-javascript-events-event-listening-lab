5947089f2753eb28b6890fc9588b42440ca83e38
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-10-13 17:49:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+//Adding event listeners
+
 function addingEventListener() {
+    const elemnt= document.querySelector('#input');
+    elemnt.addEventListener('click',addingEventListener);
+    alert('I was clicked')
 }
